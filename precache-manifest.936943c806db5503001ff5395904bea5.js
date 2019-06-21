f1c67b16d53f5ef213433bf90bc9947ac7b2976d
Logan Westbrook <westbrooklogan@gmail.com>
2019-06-21 10:21:57 -0500

    Updates

--- precache-manifest.936943c806db5503001ff5395904bea5.js
+++ precache-manifest.936943c806db5503001ff5395904bea5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "890d7035288933ea0b740c2c6b0ba90d",
+    "revision": "3419d6fd69f45caa5fc0709d66edd274",
     "url": "/kwicTest/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kwicTest/static/js/2.5911c09a.chunk.js"
   },
   {
-    "revision": "df554be1ce1509d3450d",
-    "url": "/kwicTest/static/js/main.c14620b2.chunk.js"
+    "revision": "3435064bc01542d01550",
+    "url": "/kwicTest/static/js/main.ee1324f4.chunk.js"
   },
   {
     "revision": "9490e31e99a7f3336940",
