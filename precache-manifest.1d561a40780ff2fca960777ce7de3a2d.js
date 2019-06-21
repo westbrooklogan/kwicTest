7d9b6020c70561acc62b1f65690a4be84b1d8690
Logan Westbrook <westbrooklogan@gmail.com>
2019-06-21 09:27:29 -0500

    Updates

---
+++ precache-manifest.1d561a40780ff2fca960777ce7de3a2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890d7035288933ea0b740c2c6b0ba90d",
+    "url": "/kwicTest/index.html"
+  },
+  {
+    "revision": "906d85a170cd3b8eaad5",
+    "url": "/kwicTest/static/js/2.5911c09a.chunk.js"
+  },
+  {
+    "revision": "df554be1ce1509d3450d",
+    "url": "/kwicTest/static/js/main.c14620b2.chunk.js"
+  },
+  {
+    "revision": "9490e31e99a7f3336940",
+    "url": "/kwicTest/static/js/runtime~main.8ea8a301.js"
+  }
+]);
